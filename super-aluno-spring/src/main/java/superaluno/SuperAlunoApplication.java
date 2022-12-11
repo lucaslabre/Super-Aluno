f2cd6dc389faa78535fb01815934271b41c36bdf
Lucas Labre <lucaslabre@gmail.com>
2022-12-11 19:49:55 -0300

    Criação do projeto

---
+++ super-aluno-spring/src/main/java/superaluno/SuperAlunoApplication.java
@@ -0,0 +1,13 @@
+package superaluno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperAlunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperAlunoApplication.class, args);
+	}
+
+}
---
+++ super-aluno-spring/src/test/java/superaluno/SuperAlunoApplicationTests.java
@@ -0,0 +1,13 @@
+package superaluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperAlunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
