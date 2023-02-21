cb8c7491091c8001c9c43b81c8e0d5908944ceee
Lucas Labre <lucaslabre@gmail.com>
2023-02-21 20:28:40 -0300

    Estilos do Angular Material

--- super-aluno-angular/src/app/app.module.ts
+++ super-aluno-angular/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
 import { AlunosModule } from './features/alunos/alunos.module';
 import { LoginModule } from './features/login/login.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,9 +16,11 @@ import { LoginModule } from './features/login/login.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     SharedModule,
     AlunosModule,
-    LoginModule
+    LoginModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
